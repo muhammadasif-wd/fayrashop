ee14829257181991416f6c71f811810960ba74f4
muhammadasif-wd <muhammadasif.wd@gmail.com>
2024-12-10 18:50:45 +0600

    [FEAT] - Add utility functions, schemas, and UI components for e-commerce platform

---
+++ src/components/table/data.tsx
@@ -0,0 +1,57 @@
+import { ArrowDown, ArrowUp } from "lucide-react";
+
+export const categories = [
+  {
+    value: "income",
+    label: "Income",
+  },
+  {
+    value: "food",
+    label: "Food",
+  },
+  {
+    value: "utilities",
+    label: "Utilities",
+  },
+  {
+    value: "housing",
+    label: "Housing",
+  },
+  {
+    value: "health",
+    label: "Health",
+  },
+  {
+    value: "transport",
+    label: "Transport",
+  },
+  {
+    value: "work",
+    label: "Work",
+  },
+  {
+    value: "entertainment",
+    label: "Entertainment",
+  },
+  {
+    value: "education",
+    label: "Education",
+  },
+  {
+    value: "gifts",
+    label: "Gifts",
+  },
+];
+
+export const incomeType = [
+  {
+    label: "Income",
+    value: "income",
+    icon: ArrowUp,
+  },
+  {
+    label: "Expense",
+    value: "expense",
+    icon: ArrowDown,
+  },
+];
